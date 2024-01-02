84e39a6b48f798645cc4ec8d52016b0794a8fb42
Nelson Steve <nelsonsteve799@gmail.com>
2024-01-02 23:09:27 +0500

    using poll to block events

--- src/editor.rs
+++ src/editor.rs
@@ -1,22 +1,28 @@
-use crossterm::event::{read, KeyCode, Event, KeyEventKind, KeyModifiers};
+use std::time::Duration;
+
+use crossterm::event::{poll, read, KeyCode, Event, KeyEventKind, KeyModifiers};
 
 pub struct Editor {}
 
 impl Editor {
     pub fn run(&self){
+        let mut count = 0;
         loop {
-            match read().unwrap(){
-                Event::Key(event) => {
-                    if event.kind == KeyEventKind::Press {
-                        if event.modifiers.contains(KeyModifiers::CONTROL) && event.code == KeyCode::Char('c'){
-                            break;
+            count += 1;
+            if let Ok(true) = poll(Duration::from_millis(100)){
+                match read().unwrap(){
+                    Event::Key(event) => {
+                        if event.kind == KeyEventKind::Press {
+                            if event.modifiers.contains(KeyModifiers::CONTROL) && event.code == KeyCode::Char('c'){
+                                break;
+                            }
+                            else {
+                                println!("{:?}", event);
+                            }
                         }
-                        else {
-                            println!("{:?}", event);
-                        }
-                    }
-                },
-                _ => todo!(),
+                    },
+                    _ => todo!(),
+                }
             }
         }
     }
